124a0b5b69bffd40ddae0f4429d845882ea140e3
Hang2006 <4b3g0006@office.stust.edu.tw>
2024-11-12 11:46:16 +0800

    輸入一個正整數球種整數的和

--- Exam/Exam.cpp
+++ Exam/Exam.cpp
@@ -1,7 +1,40 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n = 1;
+	int sum = 0;
+	cout << "請輸入一個正整數n: ";
+	cin >> n;
+	
+	// for loop
+	//for (int i = 1; <= n; i++) {
+	//    if (i % 2 == 0) sum += i;
+	//}
+
+	//for (int i = 2; i <= n; i += 2) {
+	// sum += i;
+	//}
+
+	// while loop
+	int i = 1;
+	sum = 0;
+	while (i <= n) {
+		if (i % 2 == 0)sum += i;
+		i++;
+	}
+
+	//while (i <= n) {
+	cout << "1~" << n << "之間所有偶數的和為: " << sum << endl;
+
+	// do-while loop
+	i = 2;
+	sum = 0;
+	do {
+		sum += i;
+		i += 2;
+	} while (i <= n);
+	cout << "Do-While loop: 1~" << n << "之間所有偶數的和: " << sum << endl;
 }
 
