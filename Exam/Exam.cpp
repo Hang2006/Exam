51d5b99d59eef8737862384aa5b0959d2edcfc3c
Hang2006 <4b3g0006@office.stust.edu.tw>
2024-11-12 12:02:56 +0800

    遞迴版本

--- Exam/Exam.cpp
+++ Exam/Exam.cpp
@@ -1,7 +1,25 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+
+	cout << "輸入一個正整數: ";
+	cin >> n;
+	
+	// 非遞迴計算n!
+	int result = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		result *= i;
+		//result = result * i;
+	}
+	cout << n << "! = " << result << endl;
+
+	// 遞迴計算n!
+	result = factorial(num); 
+	cout << "遞迴版本 : " << num << "!= " << result << endl;
+
 }
 
